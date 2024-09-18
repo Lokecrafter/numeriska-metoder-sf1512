c745daa8a4792222bb38db34a43563ab35a2155a
Lokecrafter <loke@musteryd.se>
2024-09-18 18:26:24 +0200

    fhufhiae

--- lab1/uppg6.m
+++ lab1/uppg6.m
@@ -1,9 +1,25 @@
 close all, clear all, clc;
 
 integrand = @(x) (1 - exp(-(x/3).^3)) ./ (4*x.^3);
+integrand = @(x) (1 - exp(-(x/3).^3)) ./ (4*x.^3);
 
 
 
 x = linspace(0, 10^(-4), 10000);
 
 plot(x, integrand(x));
+hold on;
+plot([0,0], [0,10^(-4)]);
+
+
+%Integranden oscillerar jättemycket. Detta beror troligen på trunkeringsfel i datorn eftersom värdet är så litet på täljaren och nämnaren i integranden.
+
+%Vi integrerar till x-värdet B. Vi hittar en integrand som är större än eller lika med ursprungliga integranden och sen beräknar vi felet vi får av att klippa av integralen.
+%Vi kan inte gå från noll heller.
+
+upper_limit = 10;
+lower_limit = 10^-(10);
+
+x = linspace(lower_limit, upper_limit, 100);
+
+trapz(x, integrand(x))
