a5a618276fb8fd6a5b1ba6511c158ab10f087310
esterarje <1gc.arje@gmail.com>
2024-09-26 19:10:08 +0200

    fixad så datorn inte dör

--- lab2/uppg9.m
+++ lab2/uppg9.m
@@ -21,12 +21,12 @@ function ret = do_euler(f, start_x, start_y, step_size, end_x, tolerance)
 
     prev_end_y = 0;
 
-    for i = 1:100
+    for i = 1:10
         x = start_x:step_size:end_x;
         y = zeros(size(x));
         y(1) = start_y;
 
-        % disp(["Number of steps: ", length(x)]);
+        % disp(["Number of steps: ", length(x)]); % Control
 
         for n = 1:length(y)
             y(n+1) = y(n) + step_size * f(y(n), x(n));
@@ -34,30 +34,38 @@ function ret = do_euler(f, start_x, start_y, step_size, end_x, tolerance)
 
         ret = y;
         
-        
         error = abs(prev_end_y - y(end));
 
-        % Exit condition based on tolerance.
+        if(i == 1)
+            disp('    x         y       step_size   error')
+        end
+        disp([x(end), y(end), step_size, error])
+
+        % Exit condition based on tolerance
         if error < tolerance
             if(i ~= 1)
                 break;
             end
         end
-        
+
         prev_end_y = y(end);
-        step_size = step_size * 0.5; % 0.5 = 1/2. Datorn räknar multiplikation snabbare än division
+        step_size = step_size * 0.5; % 0.5 = 1/2. Multiplication is faster than division.
     end
 end
+
 
 
 % Run program
 % ------------------------------------------------
 
-result = do_euler(f, 0, 2.5, 0.5, 6, 10^(-4));
+result = do_euler(f, 0, 2.5, 0.5, 6, 10^(-6)); % Making the tolerance even smaller makes the error smaller which in 
+% turn prevents further error propagation throughout the rest of the methods below. 
+% Thus making us trust at-least four (4) significant digits in the final answer. 
 x = linspace(0,6,length(result));
 step_size = 6/(length(result)-1); % Interval of x [0,6] divided by the length of 'result's (i.e number of steps)
 disp(['Last y-term: ', num2str(result(end))])
 disp(['Step size: ', num2str(step_size)])
+
 
 
 % Plot of do_euler
@@ -72,20 +80,24 @@ ylabel('y');
 grid on;
 title('Eulers metod')
 
+
+
 % Integral/volume
 % ------------------------------------------------
 
 integrand = result.*result;
-% trapz_integral = step_size * (sum(result) - (result(1) + result(length(result))) * 0.5);
-% disp(['Trapz integral value: ', num2str(trapz_integral)]);
+trapz_integral = step_size * (sum(result) - (result(1) + result(length(result))) * 0.5);
+disp(['Trapz integral (area) value: ', num2str(trapz_integral)]);
 % Area looked as to be about 3/10 of the plot-square (2.5*6) which gives an area of 4.5. 
 % Only calculating the area of the integral with Trapz's method gives the value 4.5647; we trust this value.
 volume = pi * step_size * (sum(integrand) - (integrand(1) + integrand(length(integrand))) * 0.5);
-disp(['Trapz integral (volume) value: ', num2str(volume)])
+disp(['Trapz integral (rotational volume) value: ', num2str(volume)])
+
+
 
 %-----------------------------------------------
 
-% DELUPPGIFT B
+% DELUPPGIFT D
 
 %-----------------------------------------------
 
