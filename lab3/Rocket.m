69a39a57ecd708debc72563dbf73bdc04da93837
hkallner <hilda.kallner2003@gmail.com>
2024-11-04 15:06:34 +0100

    update

--- lab3/Rocket.m
+++ lab3/Rocket.m
@@ -4,22 +4,24 @@ classdef Rocket
        y_pos            {mustBeNumeric}
        x_vel            {mustBeNumeric}
        y_vel            {mustBeNumeric}
-       mass             {mustBeNumeric}
-       min_mass         {mustBeNumeric}
-       mass_velocity    {mustBeNumeric}
+       fuel_mass        {mustBeNumeric}
+       body_mass        {mustBeNumeric}
+       burn_time        {mustBeNumeric}
        air_resistance   {mustBeNumeric}
+       force            {mustBeNumeric}
     end
     methods
-        function obj=Rocket(x_pos0,y_pos0,x_vel0,y_vel0,mass0_g, new_min_mass_g,new_mass_velocity_g_per_s,new_air_resistance)
-            if nargin == 8
+        function obj=Rocket(x_pos0,y_pos0,x_vel0,y_vel0,fuel_mass_g, body_mass_g,new_burn_time_s,new_air_resistance,new_force)
+            if nargin == 9
                 obj.x_pos = x_pos0;
                 obj.y_pos = y_pos0;
                 obj.x_vel = x_vel0;
                 obj.y_vel = y_vel0;
-                obj.mass = mass0_g;
-                obj.min_mass = new_min_mass_g;
-                obj.mass_velocity = new_mass_velocity_g_per_s;
+                obj.fuel_mass = fuel_mass_g;
+                obj.body_mass = body_mass_g;
+                obj.burn_time = new_burn_time_s;
                 obj.air_resistance = new_air_resistance;
+                obj.force=new_force;
             end
         end
     end
--- lab3/main.m
+++ lab3/main.m
@@ -1,24 +1,44 @@
 clear all; clc; close all;
 import Rocket.*;
 
-rocket1=Rocket(0,0,0,0,65,20,-15,0.01)
-rocket2=Rocket(0,0,0,0,17,5,-3,0.001)
+global rocket1;
+global rocket2;
 
-rocket1.x_pos
+rocket1=Rocket(0,0,0,0,45,20,3,0.01,10);
+rocket2=Rocket(0,0,0,0,12,5,4,0.001,3);
 
+u_0=[rocket1.x_pos;rocket1.y_pos;rocket1.x_vel;rocket1.y_vel];
 
-% classdef BasicClass
-%     properties
-%        Value {mustBeNumeric}
-%     end
-%     methods
-%        function r = roundOff(obj)
-%           r = round([obj.Value],2);
-%        end
-%        function r = multiplyBy(obj,n)
-%           r = [obj.Value]*n;
-%        end
-%     end
-% end
+function ret=odefun(t,u)
+    global rocket1;
+    global rocket2;
 
+    ret=[0;0;0;0];
+    ret(1)=u(3);
+    ret(2)=u(4);
 
+    V=hypot(u(3),u(4));
+    K=rocket1.air_resistance;
+    F=rocket1.force*(t<=rocket1.burn_time); %istället för en if-sats
+    mass=rocket1.body_mass+rocket1.fuel_mass*max(0, (rocket1.burn_time-t)/rocket1.burn_time);
+    mass=mass/1000; %massan i kilo
+    angle=0;
+    if u(3)==0 && u(4) == 0 
+        angle=deg2rad(80);
+    else
+        angle=atan2(u(4),u(3));
+    end
+
+    ret(3)=F*cos(angle)-K*u(3)*V;
+    ret(4)=F*sin(angle)-K*u(4)*V-mass*9.82;
+    ret(3)=ret(3)/mass;
+    ret(4)=ret(4)/mass;
+end
+
+tspan=[0,30];
+
+result=ode45(@odefun,tspan,u_0);
+%plot(result.x,result.y,'o-')
+plot(result.y(1,:),result.y(2,:),'-o') %y(1) är raketens x-position, y(2) är raketens y-position
+hold on 
+plot([0,100],[0,0])
