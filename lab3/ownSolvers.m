9393e66dbca940f33b0d991de344acef86e1cc61
Lokecrafter <loke@musteryd.se>
2024-11-14 10:55:01 +0100

    haha tog bort main

--- lab3/ownSolvers.m
+++ lab3/ownSolvers.m
@@ -1,18 +1,37 @@
 function ret = own_ode45(odefun, x_span, y0, number_of_steps, tolerance)
-    n = 10;
-    h = (x_span(2) - x_span(1)) / n;
-    xx = x_span(1):h:x_span(2);
-    yy = zeros(length(y0), length(xx));
-    yy(1) = y0;
+    n = number_of_steps;
+    num_equations = length(y0);
+    prev_last_y = y0;
 
-    for i = 1:(n-1)
-        k1 = odefun(xx(i), yy(i));
-        k2 = odefun(xx(i) + h * 0.5, yy(i) + h * k1 * 0.5);
-        k3 = odefun(xx(i) + h * 0.5, yy(i) + h * k2 * 0.5);
-        k4 = odefun(xx(i) + h,       yy(i) + h * k3      );
+    for iteration = 1:20
+        h = (x_span(2) - x_span(1)) / n;
+        xx = x_span(1):h:x_span(2);
+        yy = zeros(num_equations, length(xx));
+        yy(1) = y0;
 
-        K = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-        y(i+1) = y(i) + K*h;
+        %Do Runge-Kutta solve
+        for i = 1:(n-1)
+            k1 = odefun(xx(i), yy(i));
+            k2 = odefun(xx(i) + h * 0.5, yy(i) + h * k1 * 0.5);
+            k3 = odefun(xx(i) + h * 0.5, yy(i) + h * k2 * 0.5);
+            k4 = odefun(xx(i) + h,       yy(i) + h * k3      );
+
+            K = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+            yy(:,i+1) = yy(:,i) + K*h;
+        end
+
+        y_diff = yy(:,end) - prev_last_y;
+        E_trunk = norm(y_diff);
+
+        %Exit condition
+        if iteration >= 2
+            if E_trunk < tolerance
+                break
+            end
+        end
+
+        n = n * 2;
+        prev_last_y(yy(:,end))
     end
 end
 
